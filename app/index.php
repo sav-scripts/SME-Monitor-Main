409c7883701c6bbbb4f1fc8d9376340900fe29fb
sav-scripts <savorksp@hotmail.com>
2017-09-19 23:27:18 +0800

    update

--- app/index.php
+++ app/index.php
@@ -31,28 +31,8 @@ if(!in_array($_SERVER['HTTP_HOST'], $test_hosts))
 
     <link rel="stylesheet" href="styles/main.css">
 
-    <!--
-    <script>
 
-        var _gaId = "<?=$gaCode?>";
-
-        if(_gaId)
-        {
-
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-            ga('create', _gaId, 'auto');
-            ga('send', 'pageview', 'Load');
-        }
-
-
-    </script>
-    -->
     <!-- Global Site Tag (gtag.js) - Google Analytics -->
-
     <script async src="https://www.googletagmanager.com/gtag/js?id=<?=$gaCode?>"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
